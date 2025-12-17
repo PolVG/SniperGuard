cc4217f7a2d3b3749a1872b1880018c6ffcace17
Pol <pol.vidal@estudiant.enti.cat>
2025-12-17 09:36:15 +0100

    SNIP-22 Afegir color i millor visualització
    -Ordenar i millorar artisticament interficie gràfica de la GUI.

--- GUI.py
+++ GUI.py
@@ -1,33 +1,26 @@
 import tkinter as tk
-from tkinter import messagebox
 
 root = tk.Tk()
-root.title("Aviso")
-root.geometry("800x200")
+root.title("SniperGuard - Alpha GUI")
+root.geometry("500x400")
+root.resizable(False, False)  # desactiva el redimensionament (amplada, alçada)
+root.configure(bg="#1e1e1e")  # fons de la finestra
+
+# Configuració de la graella
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=1)
+root.grid_columnconfigure(2, weight=1)
 
 
-logs = tk.Label(root, text='Logs', font=("Arial", 13))
-logs.grid(row=1, column=1)
+logs = tk.Label(root, text='SNIPERGUARD', font=("Arial", 20), bg="#1e1e1e", fg="white")
+logs.grid(row=1, column=1, pady=10)
 
-arch_encontrados = tk.Label(root, text='Archivos encontrados', font=("Arial", 13))
-arch_encontrados.grid(row=1, column=2)
 
-arch_eliminados = tk.Label(root, text='Archivos eliminados', font=("Arial", 13))
 
-arch_conservados = tk.Label(root, text='Archivos conservados', font=("Arial", 13))
 
-def aviso_eliminar():
-    arch_eliminados.grid(row=3, column=1, columnspan = 2)
-    arch_conservados.grid_forget()
 
-def aviso_conservar():
-    arch_conservados.grid(row=3, column=1, columnspan = 2)
-    arch_eliminados.grid_forget()
-    
+boton_analisis_rapido = tk.Button(root, text = "Anàlisi Ràpid",height=2, width=20, bg="#2980b9", fg="black", activebackground="#3498db", borderwidth=0)
+boton_analisis_rapido.grid(row=2, column=1,pady=30)
 
-btn_el = tk.Button(root, text = "Eliminar", command = aviso_eliminar)
-btn_el.grid(row=2, column=1)
-btn_cons = tk.Button(root, text = "Conservar", command = aviso_conservar)
-btn_cons.grid(row=2, column=2)
 
 root.mainloop()
