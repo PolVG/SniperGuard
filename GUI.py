7416d78f33de194e146c9f2030c538427f684631
Artur <artur.abrahamyan@estudiant.enti.cat>
2025-12-17 22:44:59 +0100

    SNIP-21
    Interfaz gráfica para la entrega ALPHA, contiene el logo y el banner de la marca, junto a los botones de las funcionalidades que se implementarán, una barra de progreso y dos labels  para mostrar toda la información

--- GUI.py
+++ GUI.py
@@ -1,4 +1,104 @@
 import tkinter as tk
+from PIL import Image, ImageTk
+import time
+from tkinter import ttk, messagebox
+
+window = tk.Tk()
+window.title("SniperGuard")
+window.geometry("800x900")
+window.resizable(False, False)
+window.configure(bg="gray20")
+
+window.columnconfigure(1, weight=1)
+window.rowconfigure(1, weight=1)
+
+# barra Lateral
+barra_lateral = tk.Frame(window, bg="#9D9CA8", width=150)
+barra_lateral.grid(row=0, column=0, rowspan=2, sticky='ns')
+barra_lateral.grid_propagate(False)
+
+# Funcion para evitar codigo repetido para cada boton
+def crear_boton(ruta, texto_log, fila):
+    img = Image.open(ruta).resize((70, 70), Image.LANCZOS)
+    img_tk = ImageTk.PhotoImage(img)
+    btn = tk.Button(barra_lateral, image=img_tk, bg="#9D9CA8", relief="flat", command=lambda: print(texto_log))
+    btn.image = img_tk
+    btn.grid(row=fila, column=0, pady=10, padx=40, sticky='ew')
+    return btn
+
+crear_boton("SniperGuardLogo.png", "Home", 0)
+crear_boton("cleaner_sin_fondo.png", "CLEANER clicked", 1)
+crear_boton("registry_sin_nombre.png", "REGISTRY clicked", 2)
+crear_boton("herramientas_sin_fondo.png", "HERRAMIENTAS clicked", 3)
+crear_boton("opciones_sin_fondo.png", "OPCIONES clicked", 4)
+
+# banner de sniperguard
+banner = Image.open("banersinlogo.png")
+banner_redimensionado = banner.resize((650, 100), Image.LANCZOS)
+imagen_banner = ImageTk.PhotoImage(banner_redimensionado)
+label_del_banner = tk.Label(window, image=imagen_banner, bg="gray20")
+label_del_banner.grid(row=0, column=1, sticky='nwe')
+label_del_banner.image = imagen_banner
+
+
+contenedor_principal = tk.Frame(window, bg="gray20")
+contenedor_principal.grid(row=1, column=1, sticky="nsew", padx=20)
+
+
+progreso = ttk.Progressbar(contenedor_principal, orient="horizontal", length=500, mode="determinate")
+progreso.grid(row=0, column=0, pady=(20, 0))
+
+label_porcentaje = tk.Label(contenedor_principal, text="0%", bg="gray20", fg="#9D9CA8", font=("Impact", 15, "bold"))
+label_porcentaje.grid(row=1, column=0, pady=5)
+
+#funcion falseada para la barra de progreso
+def iniciar_carga():
+    for i in range(101):
+        time.sleep(0.02)
+        progreso['value'] = i
+        label_porcentaje.config(text=f"{i}%")
+        window.update_idletasks()
+
+boton_iniciar = tk.Button(contenedor_principal, text="Iniciar Escaneo", command=iniciar_carga, font=("Arial", 12, "bold"))
+boton_iniciar.grid(row=2, column=0, pady=10)
+
+
+arch_encontrados = tk.Text(contenedor_principal, bg="#D3D3D3", height=8, width=60, font=("Arial", 11))
+arch_encontrados.grid(row=3, column=0, pady=10)
+
+
+frame_botones_accion = tk.Frame(contenedor_principal, bg="gray20")
+frame_botones_accion.grid(row=4, column=0, pady=5)
+
+arch_eliminados = tk.Label(contenedor_principal, text='Archivos eliminados', font=("Arial", 11), bg="gray20", fg="red")
+arch_conservados = tk.Label(contenedor_principal, text='Archivos conservados', font=("Arial", 11), bg="gray20", fg="green")
+
+def aviso_eliminar():
+    arch_conservados.grid_forget()
+    pregunta = messagebox.askquestion('Eliminacion', 'Los siguientes archivos seran eliminados. \nEsta seguro ?')
+    if pregunta == 'yes':
+        arch_eliminados.grid(row=6, column=0, pady=5)
+
+def aviso_conservar():
+    arch_conservados.grid(row=6, column=0, pady=5)
+    arch_eliminados.grid_forget()
+
+btn_el = tk.Button(frame_botones_accion, text="Eliminar", command=aviso_eliminar, width=15)
+btn_el.grid(row=0, column=0, padx=10)
+
+btn_cons = tk.Button(frame_botones_accion, text="Conservar", command=aviso_conservar, width=15)
+btn_cons.grid(row=0, column=1, padx=10)
+
+logs = tk.Text(contenedor_principal, bg="#D3D3D3", height=8, width=60, font=("Arial", 11))
+logs.grid(row=5, column=0, pady=10)
+
+window.mainloop()
+
+
+
+
+'''
+import tkinter as tk
 from tkinter import messagebox
 
 root = tk.Tk()
@@ -58,3 +158,4 @@ btn_cons.grid(row=3, column=2, pady=10)
 
 
 root.mainloop()
+'''
